5f09a912bd651f39976e9920abf23c4c09d54307
hamerski-n <58872340+hamerski-n@users.noreply.github.com>
2020-02-22 09:34:26 -0800

    Updates

---
+++ precache-manifest.6cf05cdd61f6392b49ffa3daeee9574e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b97f9b3f39b2928ed378d841e0ad0c",
+    "url": "/react-pasv-app/index.html"
+  },
+  {
+    "revision": "97f4588743f54d4778a2",
+    "url": "/react-pasv-app/static/css/main.04325de8.chunk.css"
+  },
+  {
+    "revision": "c5445ae54680679fc29c",
+    "url": "/react-pasv-app/static/js/2.d17e0074.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-pasv-app/static/js/2.d17e0074.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f4588743f54d4778a2",
+    "url": "/react-pasv-app/static/js/main.f7845520.chunk.js"
+  },
+  {
+    "revision": "ae3d66582a74c7f2117f",
+    "url": "/react-pasv-app/static/js/runtime-main.ae68b153.js"
+  }
+]);
--- src/components/logo/logo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import logo from './react-logo.png';
-import './logo.css';
-
-function Logo() {
-    return <img className="logo" src={logo} alt='react-logo'/>;
-}
-
-export default Logo;
